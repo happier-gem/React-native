7c94228a637b61d29e23a14658dc1bd2e77ec3dc
happier-gem <gamellahappiness@gmail.com>
2026-09-10 16:14:11 +0200

    feat: update authentication navigation and layout structure

--- app/index.tsx
+++ app/index.tsx
@@ -11,10 +11,10 @@ export default function App() {
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
       Go to onboarding
       </Link>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">
       Go to Sign In
       </Link>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
       Go to Sign Up
       </Link>
     </View>
