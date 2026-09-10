db6097b16f169e5100e97ef7ccabf6fea8259b37
happier-gem <gamellahappiness@gmail.com>
2026-09-10 12:20:18 +0200

    feat: add authentication screens and update main app layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,22 @@
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function App() {
-  return React.createElement(
-    View,
-    { className: "flex-1 items-center justify-center bg-white" },
-    React.createElement(
-      Text,
-      { className: "text-xl font-bold text-blue-500" },
-      "Welcome to Nativewind!"
-    )
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      Go to onboarding
+      </Link>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      Go to Sign In
+      </Link>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+      Go to Sign Up
+      </Link>
+    </View>
   );
 }
