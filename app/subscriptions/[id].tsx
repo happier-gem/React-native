f34cdcda94276e58330577603503c5a12b4f1529
happier-gem <gamellahappiness@gmail.com>
2026-09-15 16:00:56 +0200

    feat: add new tab layout with insights, settings, and subscriptions screens; implement font loading and remove unused components

--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -8,7 +8,7 @@ const SubscriptionDetails = () => {
     return (
     <View>
       <Text> Subscription Details: {id} </Text>
-      <Link href="/tabs/subscriptions">Go back</Link>
+      <Link href="/subscriptions">Go back</Link>
      
     </View>
   )
